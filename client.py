a80132b4955787c89deabbc7e54ade0bd7f0bbe5
Johnathan <Johnathan.Lemma@lakotastudents.com>
2020-07-29 18:32:57 -0400

    fixed sending command bug

--- client.py
+++ client.py
@@ -88,8 +88,7 @@ while True:
         break
 
     while True:
-        command = s.recv(1024)
-        command = command.decode()
+        command = recv_one_message(s)
         print("command recieved")
         if int(float(command)) == 1231121288:
 
